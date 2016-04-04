e1bb145aa0e57237db5da36224c4587ce74ca727
Brian Ardiles <ardiles.brian@gmail.com>
2016-04-03 23:03:17 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+/*Insertar jquery, mas comodo seleccionar D:*/
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-2.2.2.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+/*Seleccion de xpath / parents*/
+var srcIG = $("body > div:nth-child(9) > div > div._g1ax7 > div > article > div:nth-child(2) > div > div._jjzlb > img").attr("src");
+var srcVIDEOIG = $("body > div:nth-child(9) > div > div._g1ax7 > div > article > div.ResponsiveBlock > div:nth-child(1) > div > div > div._tqoyh._pwe27 > div > video").attr("src");
+
+/*Si la variable img no tiene nada, quiere decir que es un video, por lo tanto utiliza el ink del video*/
+if (srcIG == null && srcVIDEOIG != null) {
+	window.open(srcVIDEOIG, '_blank');
+}
+
+/*Si no, usa el de la imagen =)*/
+else {
+	window.open(srcIG, '_blank');
+}
