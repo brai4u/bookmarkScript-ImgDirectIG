7fddfc151d6a3a139290e8a010174fb14156f5d8
Brian Ardiles <ardiles.brian@gmail.com>
2016-04-04 00:22:57 -0300

    fix #2

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ document.getElementsByTagName('head')[0].appendChild(script);
 
 /*Seleccion de xpath / parents*/
 var srcIG = $("body > div:nth-child(9) > div > div._g1ax7 > div > article > div:nth-child(2) > div > div._jjzlb > img").attr("src");
+
+if(srcIG == null){
+	var srcIG = $("#react-root > section > main > div > div > article > div:nth-child(2) > div > div._jjzlb > img").attr("src");
+}
+
 var srcVIDEOIG = $("video[class*='_c8hkj']").attr("src");
 
 /*Si la variable img no tiene nada, quiere decir que es un video, por lo tanto utiliza el ink del video*/
