7df3b4333edf4109b627512bfff2ed9148a94169
Brian Ardiles <ardiles.brian@gmail.com>
2017-06-19 10:02:26 -0300

    Codigo mas lindo, updateado para el sereno

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-/*Insertar jquery, mas comodo seleccionar D:*/
+/*Actualizado para vos sereno querido*/
 var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-2.2.2.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-/*Seleccion de xpath / parents*/
-var srcIG = $("body > div:nth-child(9) > div > div._g1ax7 > div > article > div:nth-child(2) > div > div._jjzlb > img").attr("src");
+/* Selector actualizado para el nuevo diseño de instagram*/
+var srcIG = $("body > div:nth-child(9) > div > div._g1ax7 > div > article > div._h5v2a > div > div > div._jjzlb > img").attr("src");
 
-if(srcIG == null){
-	var srcIG = $("#react-root > section > main > div > div > article > div:nth-child(2) > div > div._jjzlb > img").attr("src");
+if(!srcIG){
+	var srcIG = $("#react-root > section > main > div > div > article > div._h5v2a._kqf30 > div > div > div._jjzlb > img").attr("src");
 }
 
 var srcVIDEOIG = $("video[class*='_c8hkj']").attr("src");
 
 /*Si la variable img no tiene nada, quiere decir que es un video, por lo tanto utiliza el ink del video*/
-if (srcIG == null && srcVIDEOIG != null) {
+if (!srcIG && srcVIDEOIG) {
 	window.open(srcVIDEOIG, '_blank');
 }
-
-/*Si no, usa el de la imagen =)*/
 else {
 	window.open(srcIG, '_blank');
 }
